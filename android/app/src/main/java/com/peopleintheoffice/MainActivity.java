29d1ac0bfd84442dc893066fc81398aba898c8a2
catalinmiron <mironcatalin@gmail.com>
2016-10-03 14:13:01 +0300

    Add initial commit

---
+++ android/app/src/main/java/com/peopleintheoffice/MainActivity.java
@@ -0,0 +1,46 @@
+package com.peopleintheoffice;
+
+import android.os.Bundle;
+
+import com.facebook.react.LifecycleState;
+import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.modules.core.DefaultHardwareBackBtnHandler;
+import com.facebook.react.shell.MainReactPackage;
+import com.syarul.rnlocation.RNLocation;
+
+public class MainActivity extends ReactActivity implements DefaultHardwareBackBtnHandler {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "PeopleInTheOffice";
+    }
+
+    private ReactRootView mReactRootView;
+    private ReactInstanceManager mReactInstanceManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mReactRootView = new ReactRootView(this);
+
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setBundleAssetName("index.android.bundle")
+                .setJSMainModuleName("index.android")
+                .addPackage(new MainReactPackage())
+                .addPackage(new RNLocation()) // <-- Register package here
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+
+        mReactRootView.startReactApplication(mReactInstanceManager, "PeopleInTheOffice", null);
+
+        setContentView(mReactRootView);
+    }
+}
